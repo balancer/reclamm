0e3c9f6187b8ef6aee191032ed6f616eaba21fba
João Bruno <joaobrunoah@gmail.com>
2025-03-26 17:03:55 -0300

    Document math

--- contracts/lib/ReClammMath.sol
+++ contracts/lib/ReClammMath.sol
@@ -38,6 +38,18 @@ library ReClammMath {
     //    then `x = 100%/(1 - pow(2, 1/(86400+1)))`, which is 124649.
     uint256 private constant _SECONDS_PER_DAY_WITH_ADJUSTMENT = 124649;
 
+    /**
+     * @notice Get the current virtual balances and compute the invariant of the pool using constant product.
+     * @param balancesScaled18 Current pool balances, sorted in token registration order
+     * @param lastVirtualBalances The last virtual balances, sorted in token registration order
+     * @param timeConstant IncreaseDayRate divided by 124649
+     * @param lastTimestamp The timestamp of the last user interaction with the pool
+     * @param currentTimestamp The current timestamp
+     * @param centerednessMargin A limit of the pool centeredness that defines if pool is out of range
+     * @param sqrtPriceRatioState A struct containing start and end price ratios and a time interval
+     * @param rounding Rounding direction to consider when computing the invariant
+     * @return invariant The invariant of the pool.
+     */
     function computeInvariant(
         uint256[] memory balancesScaled18,
         uint256[] memory lastVirtualBalances,
@@ -47,7 +59,7 @@ library ReClammMath {
         uint256 centerednessMargin,
         SqrtPriceRatioState storage sqrtPriceRatioState,
         Rounding rounding
-    ) internal pure returns (uint256) {
+    ) internal pure returns (uint256 invariant) {
         (uint256[] memory currentVirtualBalances, ) = getCurrentVirtualBalances(
             balancesScaled18,
             lastVirtualBalances,
@@ -61,6 +73,20 @@ library ReClammMath {
         return computeInvariant(balancesScaled18, currentVirtualBalances, rounding);
     }
 
+    /**
+     * @notice Compute the invariant of the pool using constant product.
+     * @dev Notice that the invariant is computed as (x+a)(y+b), without square root. This is because the calculations
+     * of virtual balance updates are easier with this invariant. Different from other pools, the invariant of ReClamm
+     * will change over time if pool is out of range or price ratio is updating, so the pool is not composable.
+     * Therefore, the BPT value is meaningless. Moreover, only add/remove liquidity proportional is supported, which
+     * does not require the invariant. So, it does not matter if the invariant and liquidity relation is not linear,
+     * and the invariant is used mostly to calculate swaps.
+     *
+     * @param balancesScaled18 Current pool balances, sorted in token registration order
+     * @param virtualBalances The last virtual balances, sorted in token registration order
+     * @param rounding Rounding direction to consider when computing the invariant
+     * @return invariant The invariant of the pool
+     */
     function computeInvariant(
         uint256[] memory balancesScaled18,
         uint256[] memory virtualBalances,
@@ -73,37 +99,53 @@ library ReClammMath {
         return _mulUpOrDown((balancesScaled18[0] + virtualBalances[0]), (balancesScaled18[1] + virtualBalances[1]));
     }
 
+    /**
+     * @notice Compute the `amountOut` of tokenOut in a swap, given the current balances and virtual balances.
+     * @param balancesScaled18 Current pool balances, sorted in token registration order
+     * @param virtualBalances The last virtual balances, sorted in token registration order
+     * @param tokenInIndex Index of the token being swapped in
+     * @param tokenOutIndex Index of the token being swapped out
+     * @param amountGivenScaled18 The exact amount of `tokenIn` (i.e., the amount given in an ExactIn swap)
+     * @return amountCalculatedScaled18 The calculated amount of `tokenOut` returned in an ExactIn swap
+     */
     function calculateOutGivenIn(
         uint256[] memory balancesScaled18,
         uint256[] memory virtualBalances,
         uint256 tokenInIndex,
         uint256 tokenOutIndex,
         uint256 amountGivenScaled18
-    ) internal pure returns (uint256) {
-        uint256[] memory totalBalances = new uint256[](balancesScaled18.length);
-
-        totalBalances[0] = balancesScaled18[0] + virtualBalances[0];
-        totalBalances[1] = balancesScaled18[1] + virtualBalances[1];
-
-        uint256 invariant = totalBalances[0].mulUp(totalBalances[1]);
+    ) internal pure returns (uint256 amountCalculatedScaled18) {
+        // Round up, so the swapper absorbs rounding imprecisions (rounds in favor of the vault).
+        uint256 invariant = computeInvariant(balancesScaled18, virtualBalances, Rounding.ROUND_UP);
         // Total (virtual + real) token out amount that should stay in the pool after the swap.
-        uint256 tokenOutPoolAmount = invariant.divUp(totalBalances[tokenInIndex] + amountGivenScaled18);
+        uint256 tokenOutPoolAmount = invariant.divUp(
+            balancesScaled18[tokenInIndex] + virtualBalances[tokenInIndex] + amountGivenScaled18
+        );
 
-        if (tokenOutPoolAmount > totalBalances[tokenOutIndex]) {
+        uint256 totalBalancesTokenOut = balancesScaled18[tokenOutIndex] + virtualBalances[tokenOutIndex];
+
+        if (tokenOutPoolAmount > totalBalancesTokenOut) {
             // If the amount of `tokenOut` remaining in the pool post-swap is greater than the total balance of
             // `tokenOut`, that means the swap result is negative due to a rounding issue.
             revert NegativeAmountOut();
         }
 
-        uint256 amountOut = totalBalances[tokenOutIndex] - tokenOutPoolAmount;
-        if (amountOut > balancesScaled18[tokenOutIndex]) {
+        amountCalculatedScaled18 = totalBalancesTokenOut - tokenOutPoolAmount;
+        if (amountCalculatedScaled18 > balancesScaled18[tokenOutIndex]) {
             // Amount out cannot be bigger than the real balance of the token.
             revert AmountOutBiggerThanBalance();
         }
-
-        return amountOut;
     }
 
+    /**
+     * @notice Compute the `amountIn` of tokenIn in a swap, given the current balances and virtual balances.
+     * @param balancesScaled18 Current pool balances, sorted in token registration order
+     * @param virtualBalances The last virtual balances, sorted in token registration order
+     * @param tokenInIndex Index of the token being swapped in
+     * @param tokenOutIndex Index of the token being swapped out
+     * @param amountGivenScaled18 The exact amount of `tokenOut` (i.e., the amount given in an ExactOut swap)
+     * @return amountCalculatedScaled18 The calculated amount of `tokenIn` returned in an ExactOut swap
+     */
     function calculateInGivenOut(
         uint256[] memory balancesScaled18,
         uint256[] memory virtualBalances,
@@ -116,16 +158,22 @@ library ReClammMath {
             revert AmountOutBiggerThanBalance();
         }
 
-        uint256[] memory totalBalances = new uint256[](balancesScaled18.length);
+        // Round up, so the swapper absorbs rounding imprecisions (rounds in favor of the vault).
+        uint256 invariant = computeInvariant(balancesScaled18, virtualBalances, Rounding.ROUND_UP);
 
-        totalBalances[0] = balancesScaled18[0] + virtualBalances[0];
-        totalBalances[1] = balancesScaled18[1] + virtualBalances[1];
-
-        uint256 invariant = totalBalances[0].mulUp(totalBalances[1]);
-
-        return invariant.divUp(totalBalances[tokenOutIndex] - amountGivenScaled18) - totalBalances[tokenInIndex];
+        return
+            invariant.divUp(balancesScaled18[tokenOutIndex] + virtualBalances[tokenOutIndex] - amountGivenScaled18) -
+            balancesScaled18[tokenInIndex] -
+            virtualBalances[tokenInIndex];
     }
 
+    /**
+     * @notice Calculate the initial virtual balances of the pool.
+     * @dev The initial virtual balances are calculated based on the initial sqrt price ratio and the initial balances.
+     * @param balancesScaled18 Current pool balances, sorted in token registration order
+     * @param sqrtPriceRatio The initial sqrt price ratio of the pool
+     * @return virtualBalances The initial virtual balances of the pool
+     */
     function initializeVirtualBalances(
         uint256[] memory balancesScaled18,
         uint256 sqrtPriceRatio
@@ -135,6 +183,24 @@ library ReClammMath {
         virtualBalances[1] = balancesScaled18[1].divDown(sqrtPriceRatio - FixedPoint.ONE);
     }
 
+    /**
+     * @notice Calculate the current virtual balances of the pool.
+     * @dev If the pool is in range or the price ratio is not updating, the virtual balances do not change and
+     * lastVirtualBalances are returned. Otherwise, follow these three steps:
+     * 1. Calculate the current sqrt price ratio.
+     * 2. Shrink/Expand the price interval considering the current sqrt price ratio. (if price ratio is updating)
+     * 3. Track the market price by moving the price interval. (if pool is out of range)
+     *
+     * @param balancesScaled18 Current pool balances, sorted in token registration order
+     * @param lastVirtualBalances The last virtual balances, sorted in token registration order
+     * @param timeConstant IncreaseDayRate divided by 124649
+     * @param lastTimestamp The timestamp of the last user interaction with the pool
+     * @param currentTimestamp The current timestamp
+     * @param centerednessMargin A limit of the pool centeredness that defines if pool is out of range
+     * @param sqrtPriceRatioState A struct containing start and end price ratios and a time interval
+     * @return currentVirtualBalances The current virtual balances of the pool
+     * @return changed Whether the virtual balances have changed and must be updated in the pool
+     */
     function getCurrentVirtualBalances(
         uint256[] memory balancesScaled18,
         uint256[] memory lastVirtualBalances,
@@ -156,7 +222,6 @@ library ReClammMath {
 
         SqrtPriceRatioState memory _sqrtPriceRatioState = sqrtPriceRatioState;
 
-        // Calculate currentSqrtPriceRatio
         uint256 currentSqrtPriceRatio = calculateSqrtPriceRatio(
             currentTimestamp,
             _sqrtPriceRatioState.startSqrtPriceRatio,
@@ -167,6 +232,7 @@ library ReClammMath {
 
         bool isPoolAboveCenter = isAboveCenter(balancesScaled18, lastVirtualBalances);
 
+        // If the price ratio is updating, update
         if (
             _sqrtPriceRatioState.startTime != 0 &&
             currentTimestamp > _sqrtPriceRatioState.startTime &&
