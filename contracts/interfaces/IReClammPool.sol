b2fc7aa61ae34b1480a4ecd6ebebc0bcfb56af85
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2025-04-02 17:13:57 -0300

    Create functions to get dynamic data from ReClamm Pools (#54)
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- contracts/interfaces/IReClammPool.sol
+++ contracts/interfaces/IReClammPool.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.24;
 
 import { IBasePool } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePool.sol";
 
+import { PriceRatioState } from "../lib/ReClammMath.sol";
+
 /// @dev Struct with data for deploying a new ReClammPool.
 struct ReClammPoolParams {
     string name;
@@ -15,6 +17,10 @@ struct ReClammPoolParams {
 }
 
 interface IReClammPool is IBasePool {
+    /********************************************************   
+                           Errors
+    ********************************************************/
+
     /// @dev Indicates that the start time is after the end time.
     error GradualUpdateTimeTravel(uint256 resolvedStartTime, uint256 endTime);
 
@@ -36,6 +42,10 @@ interface IReClammPool is IBasePool {
     /// @dev The pool is out of range before or after the operation.
     error PoolIsOutOfRange();
 
+    /********************************************************
+                           Events
+    ********************************************************/
+
     /// @notice The Price Ratio State was updated.
     event PriceRatioStateUpdated(
         uint256 startFourthRootPriceRatio,
@@ -47,25 +57,66 @@ interface IReClammPool is IBasePool {
     /// @dev The Virtual Balances were updated after a user interaction.
     event VirtualBalancesUpdated(uint256[] virtualBalances);
 
-    /// @dev The Price Shift Daily Rate was updated.
-    event PriceShiftDailyRateUpdated(uint256 priceShiftDailyRate);
+    /**
+     * @dev The Price Shift Daily Rate was updated.
+     * @param priceShiftDailyRate The new price shift daily rate
+     * @param timeConstant A representation of the price shift daily rate in seconds
+     */
+    event PriceShiftDailyRateUpdated(uint256 priceShiftDailyRate, uint256 timeConstant);
 
     /// @dev The Centeredness Margin was updated.
     event CenterednessMarginUpdated(uint256 centerednessMargin);
 
+    /// @dev The timestamp of the last user interaction.
+    event LastTimestampUpdated(uint32 lastTimestamp);
+
+    /********************************************************
+                       Pool State Getters
+    ********************************************************/
+
     /**
-     * @notice Returns the current virtual balances.
+     * @notice Returns the current virtual balances and a flag indicating whether they have changed.
      * @dev The current virtual balances are calculated based on the last virtual balances. If the pool is in range
      * and the price ratio is not updating, the virtual balances will not change. If the pool is out of range or the
      * price ratio is updating, this function will calculate the new virtual balances based on the timestamp of the
      * last user interaction. Note that virtual balances are always scaled18 values.
      *
      * @return currentVirtualBalances The current virtual balances
+     * @return changed Whether the current virtual balances are different from `lastVirtualBalances`
      */
-    function getCurrentVirtualBalances() external view returns (uint256[] memory currentVirtualBalances);
+    function getCurrentVirtualBalances() external view returns (uint256[] memory currentVirtualBalances, bool changed);
 
     /// @notice Returns the timestamp of the last user interaction.
     function getLastTimestamp() external view returns (uint32);
+
+    /**
+     * @notice Getter for the last virtual balances.
+     * @return lastVirtualBalances The virtual balances at the time of the last user interaction
+     */
+    function getLastVirtualBalances() external view returns (uint256[] memory lastVirtualBalances);
+
+    /**
+     * @notice Returns the centeredness margin.
+     * @dev The centeredness margin is a symmetrical measure of how closely an unbalanced pool can approach the limits
+     * of the price range before the pool is considered out of range.
+     *
+     * @return centerednessMargin The current centeredness margin
+     */
+    function getCenterednessMargin() external view returns (uint256 centerednessMargin);
+
+    /**
+     * @notice Returns the time constant.
+     * @dev The time constant is an internal representation of the raw price shift daily rate, expressed in seconds.
+     * @return timeConstant The time constant
+     */
+    function getTimeConstant() external view returns (uint256 timeConstant);
+
+    /**
+     * @notice Returns the current price ratio state.
+     * @dev This includes start and end values for the fourth root price ratio, and start and end times for the update.
+     * @return priceRatioState The current price ratio state
+     */
+    function getPriceRatioState() external view returns (PriceRatioState memory priceRatioState);
 
     /**
      * @notice Returns the current fourth root of price ratio.
@@ -75,6 +126,10 @@ interface IReClammPool is IBasePool {
      * @return currentFourthRootPriceRatio The current fourth root of price ratio
      */
     function getCurrentFourthRootPriceRatio() external view returns (uint96);
+
+    /********************************************************
+                       Pool State Setters
+    ********************************************************/
 
     /**
      * @notice Resets the price ratio update by setting a new end fourth root price ratio and time range.
