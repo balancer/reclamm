b665cc84ea96798fd68a43bf680242d4460ef04c
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2025-04-08 13:20:52 -0300

    Swap gas tests (#60)

--- contracts/ReClammPoolFactory.sol
+++ contracts/ReClammPoolFactory.sol
@@ -42,11 +42,11 @@ contract ReClammPoolFactory is IPoolVersion, BasePoolFactory, Version {
      * @param name The name of the pool
      * @param symbol The symbol of the pool
      * @param tokens An array of descriptors for the tokens the pool will manage
+     * @param roleAccounts Addresses the Vault will allow to change certain pool settings
+     * @param swapFeePercentage Initial swap fee percentage
      * @param priceShiftDailyRate The allowed change in a virtual balance per day
      * @param fourthRootPriceRatio The fourth root of the price ratio
      * @param centerednessMargin How far the price can be from the center before the price range starts to move
-     * @param roleAccounts Addresses the Vault will allow to change certain pool settings
-     * @param swapFeePercentage Initial swap fee percentage
      * @param salt The salt value that will be passed to deployment
      */
     function create(
