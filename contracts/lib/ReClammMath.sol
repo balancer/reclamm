921033062bf83ba886b5e1887b02819ef968a103
João Bruno <joaobrunoah@gmail.com>
2025-04-09 15:48:32 -0300

    Save gas by choosing when to calculate if pool is above center

--- contracts/lib/ReClammMath.sol
+++ contracts/lib/ReClammMath.sol
@@ -230,6 +230,11 @@ library ReClammMath {
         );
     }
 
+    struct PoolAboveCenter {
+        bool isPoolAboveCenter;
+        bool isPoolAboveCenterCalculated;
+    }
+
     /**
      * @notice Calculate the current virtual balances of the pool.
      * @dev If the pool is in range or the price ratio is not updating, the virtual balances do not change and
@@ -276,7 +281,7 @@ library ReClammMath {
             _priceRatioState.priceRatioUpdateEndTime
         );
 
-        bool isPoolAboveCenter = isAboveCenter(balancesScaled18, lastVirtualBalances);
+        PoolAboveCenter memory poolAboveCenter;
 
         // If the price ratio is updating, shrink/expand the price interval by recalculating the virtual balances.
         // Skip the update if the start and end price ratio are the same, because the virtual balances are already
@@ -286,11 +291,14 @@ library ReClammMath {
             lastTimestamp < _priceRatioState.priceRatioUpdateEndTime &&
             _priceRatioState.startFourthRootPriceRatio != _priceRatioState.endFourthRootPriceRatio
         ) {
+            poolAboveCenter.isPoolAboveCenter = isAboveCenter(balancesScaled18, lastVirtualBalances);
+            poolAboveCenter.isPoolAboveCenterCalculated = true;
+
             currentVirtualBalances = calculateVirtualBalancesUpdatingPriceRatio(
                 currentFourthRootPriceRatio,
                 balancesScaled18,
                 lastVirtualBalances,
-                isPoolAboveCenter
+                poolAboveCenter.isPoolAboveCenter
             );
 
             changed = true;
@@ -298,11 +306,15 @@ library ReClammMath {
 
         // If the pool is out of range, track the market price by moving the price interval.
         if (isPoolInRange(balancesScaled18, currentVirtualBalances, centerednessMargin) == false) {
+            if (poolAboveCenter.isPoolAboveCenterCalculated == false) {
+                poolAboveCenter.isPoolAboveCenter = isAboveCenter(balancesScaled18, lastVirtualBalances);
+            }
+
             currentVirtualBalances = calculateVirtualBalancesUpdatingPriceRange(
                 currentFourthRootPriceRatio,
                 balancesScaled18,
                 currentVirtualBalances,
-                isPoolAboveCenter,
+                poolAboveCenter.isPoolAboveCenter,
                 priceShiftDailyRangeInSeconds,
                 currentTimestamp,
                 lastTimestamp
