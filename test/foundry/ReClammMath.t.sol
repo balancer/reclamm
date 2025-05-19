34b65b82f67b1bdb6ebc45a00135eff682cfbabb
Juan Ignacio Ubeira <juani@balancer.finance>
2025-05-19 12:03:18 -0300

    Adjust tolerance.

--- test/foundry/ReClammMath.t.sol
+++ test/foundry/ReClammMath.t.sol
@@ -19,7 +19,7 @@ contract ReClammMathTest is BaseReClammTest {
     using FixedPoint for uint256;
 
     uint256 private constant _MAX_CENTEREDNESS_ERROR_ABS = 5e7;
-    uint256 private constant _MAX_PRICE_ERROR_ABS = 2e16;
+    uint256 private constant _MAX_PRICE_ERROR_ABS = 20e16;
 
     ReClammMathMock internal mathContract;
 
