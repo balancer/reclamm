ca4d4185818f6258fd08f0d0f855448b67f8d46d
João Bruno <joaobrunoah@gmail.com>
2025-05-21 13:31:46 -0300

    Fix merge issues

--- contracts/ReClammPool.sol
+++ contracts/ReClammPool.sol
@@ -1040,8 +1040,4 @@ contract ReClammPool is IReClammPool, BalancerPoolToken, PoolInfo, BasePoolAuthe
     function _computeMaxPrice(uint256 currentInvariant, uint256 virtualBalanceA) internal pure returns (uint256) {
         return currentInvariant.divDown(virtualBalanceA.mulDown(virtualBalanceA));
     }
-
-    function _computeMaxPrice(uint256 currentInvariant, uint256 virtualBalanceA) internal pure returns (uint256) {
-        return currentInvariant.divDown(virtualBalanceA.mulDown(virtualBalanceA));
-    }
 }
