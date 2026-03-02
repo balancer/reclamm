fad374a82fde8d7204dd17067ebd4614e20c1346
Jeff Bennett <endymionjkb@gmail.com>
2026-03-02 14:12:55 -0500

    test: fix warning while I'm at it

--- test/foundry/ReclammPoolInit.t.sol
+++ test/foundry/ReclammPoolInit.t.sol
@@ -310,13 +310,13 @@ contract ReClammPoolInitTest is BaseReClammTest {
         // Does not revert either way.
         vm.startPrank(lp);
 
-        uint256 snapshotId = vm.snapshot();
+        uint256 snapshotId = vm.snapshotState();
         _initPool(newPool, initialBalancesRawGivenUsdc, 0);
         _validatePostInitConditions();
 
         uint256 spotPriceGivenUsdc = ReClammPool(newPool).computeCurrentSpotPrice();
 
-        vm.revertTo(snapshotId);
+        vm.revertToState(snapshotId);
         _initPool(newPool, initialBalancesRawGivenWeth, 0);
         _validatePostInitConditions();
 
